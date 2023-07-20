feaf826ff9de18867178a06670189a6d29122381
Aida Chernyavskaya <aidachernyavskaya@gmail.com>
2023-07-20 18:08:41 +0300

    Add input field

--- src/App.js
+++ src/App.js
@@ -4,9 +4,20 @@ import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <div>
-      <Navbar/>
-    </div>
+      <div>
+          <Navbar/>
+
+          <h1 className={'h1'}>Курс валют</h1>
+          <div className={'content'}>
+              <h2 className={'h2'}>Курс показан относительно базовой валюты - RUB</h2>
+              <h3 className={'h3'}>Изменить базовую валюту</h3>
+              <div className={'input'}>
+                  <input className={'input__field'} type={'text'} placeholder={'Базовая валюта'}/>
+                  <button className={'input__button'}>Изменить</button>
+              </div>
+          </div>
+
+      </div>
   );
 }
 
