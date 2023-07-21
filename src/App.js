d96b40d3c4dd73857f4ed092fb44e3a63396b690
Aida Chernyavskaya <aidachernyavskaya@gmail.com>
2023-07-21 14:39:47 +0300

    Add adaptive for exchange rate table

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
 import './styles/App.css';
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
+    const [width, setWidth] = useState(window.innerWidth);
+    window.addEventListener('resize', e => {
+        setWidth(window.innerWidth);
+    })
   return (
       <div>
           <Navbar/>
@@ -15,25 +19,54 @@ function App() {
                   <input className={'input__field'} type={'text'} placeholder={'Базовая валюта'}/>
                   <button className={'input__button'}>Изменить</button>
               </div>
-              <div className={'exchange_rate'}>
-                  <div className={'exchange_rate__column'}>
-                      <p className={'text text__bold'}>Букв. код</p>
-                      <p className={'text text__bold'}>Валюта</p>
-                      <p className={'text text__bold'}>Курс</p>
-                      <p className={'text text__bold'}>RUB</p>
-                      <p className={'text'}>Российский рубль</p>
-                      <p className={'text'}>34.45</p>
-                  </div>
-                  <div className={'hr__vertical'}></div>
-                  <div className={'exchange_rate__column'}>
-                      <p className={'text text__bold'}>Букв. код</p>
-                      <p className={'text text__bold'}>Валюта</p>
-                      <p className={'text text__bold'}>Курс</p>
-                      <p className={'text text__bold'}>RUB</p>
-                      <p className={'text'}>Российский рубль</p>
-                      <p className={'text'}>34.45</p>
-                  </div>
-              </div>
+                  {width > 700
+                  ?   <div className={'exchange_rate'}>
+                          <div className={'exchange_rate__column'}>
+                              <div className={'exchange_rate__row'}>
+                                  <p className={'text text__bold'}>Букв. код</p>
+                                  <p className={'text text__bold'}>Валюта</p>
+                                  <p className={'text text__bold'}>Курс</p>
+                              </div>
+                              <div className={'exchange_rate__row'}>
+                                  <p className={'text text__bold'}>RUB</p>
+                                  <p className={'text'}>Российский рубль</p>
+                                  <p className={'text'}>34.45</p>
+                              </div>
+                          </div>
+                          <div className={'hr__vertical'}></div>
+                          <div className={'exchange_rate__column'}>
+                              <div className={'exchange_rate__row'}>
+                                  <p className={'text text__bold'}>Букв. код</p>
+                                  <p className={'text text__bold'}>Валюта</p>
+                                  <p className={'text text__bold'}>Курс</p>
+                              </div>
+                              <div className={'exchange_rate__row'}>
+                                  <p className={'text text__bold'}>RUB</p>
+                                  <p className={'text'}>Российский рубль</p>
+                                  <p className={'text'}>34.45</p>
+                              </div>
+                          </div>
+                      </div>
+                  :   <div className={'exchange_rate'}>
+                      <div className={'exchange_rate__column'}>
+                          <div className={'exchange_rate__row'}>
+                              <p className={'text text__bold'}>Букв. код</p>
+                              <p className={'text text__bold'}>Валюта</p>
+                              <p className={'text text__bold'}>Курс</p>
+                          </div>
+                          <div className={'exchange_rate__row'}>
+                              <p className={'text text__bold'}>RUB</p>
+                              <p className={'text'}>Российский рубль</p>
+                              <p className={'text'}>34.45</p>
+                          </div>
+                          <div className={'exchange_rate__row'}>
+                              <p className={'text text__bold'}>RUB</p>
+                              <p className={'text'}>Российский рубль</p>
+                              <p className={'text'}>34.45</p>
+                          </div>
+                      </div>
+                      </div>
+                  }
           </div>
       </div>
   );
