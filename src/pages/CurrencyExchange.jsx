740d3577c05536cbed046feabe8567d7ece90435
Aida Chernyavskaya <aidachernyavskaya@gmail.com>
2023-07-24 17:57:32 +0300

    Add input field on currency exchange page

--- src/pages/CurrencyExchange.jsx
+++ src/pages/CurrencyExchange.jsx
@@ -3,7 +3,20 @@ import React from 'react';
 const CurrencyExchange = () => {
     return (
         <div>
-
+            <h1 className={'h1'}>Конвертер валют</h1>
+            <div className={'content'}>
+                <h3 className={'h3'}>
+                    Введите запрос обмена в формате <span className={'h3__alternative'}>“15 usd in rub”</span>
+                </h3>
+                <div className={'input'}>
+                    <input
+                        className={'input__field'}
+                        type={'text'}
+                        placeholder={'Запрос обмена'}
+                    />
+                    <button className={'input__button'}>Обменять</button>
+                </div>
+            </div>
         </div>
     );
 };
