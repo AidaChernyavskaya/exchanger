293dfd1e2d7d64923fc2aaa769b2b8577f827ab2
Aida Chernyavskaya <aidachernyavskaya@gmail.com>
2023-07-25 12:15:12 +0300

    Add currency exchange card layout

--- src/pages/CurrencyExchange.jsx
+++ src/pages/CurrencyExchange.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Arrows from '../icons/arrows.svg';
 
 const CurrencyExchange = () => {
     return (
@@ -15,6 +16,20 @@ const CurrencyExchange = () => {
                         placeholder={'Запрос обмена'}
                     />
                     <button className={'input__button'}>Обменять</button>
+                </div>
+
+                <div className={'card'}>
+                    <p className={'text__italic'}>У меня есть</p>
+                    <h4 className={'h4'}>15</h4>
+                    <h2 className={'h2 h2__bold'}>USD</h2>
+                </div>
+                <button className={'change__button'}>
+                    <input type={"image"} src={Arrows} className={'arrows__icon'}/>
+                </button>
+                <div className={'card'}>
+                    <p className={'text__italic'}>У меня будет</p>
+                    <h4 className={'h4'}>1340</h4>
+                    <h2 className={'h2 h2__bold'}>RUB</h2>
                 </div>
             </div>
         </div>
