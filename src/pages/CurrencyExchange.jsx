6c3a232135535bd0d4e3e70b70dba7ce068a8778
Aida Chernyavskaya <aidachernyavskaya@gmail.com>
2023-07-25 17:02:07 +0300

    Add Loader to CurrencyExchange page

--- src/pages/CurrencyExchange.jsx
+++ src/pages/CurrencyExchange.jsx
@@ -2,11 +2,13 @@ import React, {useEffect, useState} from 'react';
 import ExchangeService from "../API/ExchangeService";
 import InputField from "../components/InputField/InputField";
 import ExchangeCards from "../components/ExchangeCards/ExchangeCards";
+import Loader from "../components/Loader/Loader";
 
 const CurrencyExchange = () => {
     const [exchange,setExchange] = useState([]);
     const [request, setRequest] = useState({amount: 15, from: 'USD', to: 'RUB'});
     const [value, setValue] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         fetchCurrencyExchange();
@@ -14,9 +16,11 @@ const CurrencyExchange = () => {
 
 
     async function fetchCurrencyExchange() {
+        setIsLoading(true);
         setTimeout( async () => {
             const exchange = await ExchangeService.getCurrencyExchange(request.amount, request.from, request.to);
             setExchange(exchange);
+            setIsLoading(false);
         })
     }
 
@@ -35,6 +39,12 @@ const CurrencyExchange = () => {
         setValue('');
     }
 
+    const handleKeyPress = (event) => {
+        if (event.key === 'Enter'){
+            handleSubmit();
+        }
+    }
+
     return (
         <div>
             <h1 className={'h1'}>Конвертер валют</h1>
@@ -42,15 +52,21 @@ const CurrencyExchange = () => {
                 <h3 className={'h3'}>
                     Введите запрос обмена в формате <span className={'h3__alternative'}>“15 usd in rub”</span>
                 </h3>
+
                 <InputField
                     value={value}
                     onChange={handleChange}
                     onClick={handleSubmit}
+                    onKeyDown={handleKeyPress}
                     placeholder={'Запрос обмена'}
                 >
                     Обменять
                 </InputField>
-                <ExchangeCards amount={request.amount} from={request.from} to={request.to} result={exchange.result}/>
+
+                {isLoading
+                ? <Loader/>
+                : <ExchangeCards amount={request.amount} from={request.from} to={request.to} result={exchange.result}/>
+                }
             </div>
         </div>
     );
